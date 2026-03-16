0e657cb66d089995f31aeb533aad8235d0c1a44a
Kumar Sujal <kumarsujal123456@gmail.com>
2026-03-16 20:12:42 +0530

    mapped routes and functions for register and login forms with ability to load and get data

--- flask_blog.py
+++ flask_blog.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 # First I import Flask which is the core framework used to create the web server.
 #  I also import render_template which allows me to dynamically render HTML templates, and 
 #  url_for which helps generate URLs for routes
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 # Here I create the Flask application instance.
 #  The __name__ parameter helps Flask locate resources like templates and 
 #  static files relative to the current module.
+
+app.config['SECRET_KEY'] = 'c3136bac1cbbf1f5b906d44fd17c6c84'
+# The application is configured using Flask’s configuration system. 
+# I defined a SECRET_KEY which is required by Flask-WTF to securely handle form submissions and CSRF protection.
 
 posts = [
     {
@@ -26,7 +31,7 @@ posts = [
 
 @app.route("/") #Flask uses decorators to define routes. 
 @app.route("/home") #These routes map specific URLs to Python functions.
-def hello_world():
+def home():
     return render_template("home.html", posts=posts)
 # This function renders the home.html template and passes the posts data into the template. 
 # Flask uses the Jinja2 templating engine which allows dynamic rendering of content inside HTML.
@@ -34,6 +39,33 @@ def hello_world():
 @app.route("/about")
 def about():
     return render_template("about.html", title='About')
+
+# This route handles the user registration page. 
+# It accepts both GET and POST requests. 
+# A GET request loads the registration form, while a POST request processes the submitted form data
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # The validate_on_submit() method checks two things: 
+        # first, whether the request method is POST
+        # second, whether all the form validators pass successfully
+        flash(f'Account created for {form.username.data}!', 'success')
+        # Flash messages are used to send temporary feedback to the user after an action occurs
+        return redirect(url_for('home'))
+        # After successful form submission, the user is redirected to the home page using redirect and url_for
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
