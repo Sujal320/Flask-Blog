f64687df5a7d76641b00b5bcad51e21187960dbf
Kumar Sujal <kumarsujal123456@gmail.com>
2026-03-19 20:19:13 +0530

    integrated database and test some insertions and deletions

--- flask_blog.py
+++ flask_blog.py
@@ -1,8 +1,13 @@
+
+from datetime import datetime, timezone
 from flask import Flask, render_template, url_for, flash, redirect
 # First I import Flask which is the core framework used to create the web server.
 #  I also import render_template which allows me to dynamically render HTML templates, and 
 #  url_for which helps generate URLs for routes
 from forms import RegistrationForm, LoginForm
+
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 # Here I create the Flask application instance.
@@ -12,6 +17,31 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c3136bac1cbbf1f5b906d44fd17c6c84'
 # The application is configured using Flask’s configuration system. 
 # I defined a SECRET_KEY which is required by Flask-WTF to securely handle form submissions and CSRF protection.
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
